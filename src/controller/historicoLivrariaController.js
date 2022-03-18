0107bf8a2633fa09672e6a5f70f5a5ef1c6d90d3
yagormorares <yago.dmoraes@gmail.com>
2022-03-18 09:51:20 -0300

    Uso de async/await nas controllers

--- src/controller/historicoLivrariaController.js
+++ src/controller/historicoLivrariaController.js
@@ -4,38 +4,32 @@ import HistoricoLivrariaDAO from "../DAO/HistoricoLivrariaDAO.js"
 const historicoLivrariaController = (app,bd) =>{
     const historicoDAO = new HistoricoLivrariaDAO(bd)
 
-    app.get('/historico',(req,res)=>{
-        historicoDAO.pegarTodosHistoricos()
-        .then((resposta)=>{
+    app.get('/historico', async (req,res)=>{
+        try{
+            const resposta = await historicoDAO.pegarTodosHistoricos()
             res.json(resposta)
-        })
-        .catch((erro)=>{
+        }catch(erro){
             res.json(erro)
-        })
+        }
     })
 
-    app.get('/historico/id_user/:id_user',(req,res)=>{
-        const id_user = req.params.id_user
-        historicoDAO.pegarUmUsuario(id_user)
-        .then((resposta)=>{
+    app.get('/historico/id_user/:id_user',async (req,res)=>{
+        try {
+            const id_user = req.params.id_user
+            const resposta = await historicoDAO.pegarUmUsuario(id_user)
             res.json(resposta)
-        })
-        .catch((erro)=>{
+        } catch (erro) {
             res.json(erro)
-        })
+        }
+
     })
 
-    app.post('/historico', (req,res)=>{
+    app.post('/historico', async (req,res)=>{
         const body = req.body
         try{
             const novoHistorico = new HistoricoLivraria(body.id_user, body.item, body.preco, body.data_compra)
-            historicoDAO.inserirNovoHistorico(novoHistorico)
-            .then((resposta)=>{
-                res.json(resposta)
-            })
-            .catch((erro)=>{
-                res.json(erro)
-            })
+            const resposta = await historicoDAO.inserirNovoHistorico(novoHistorico)
+            res.json(resposta)
         }catch(error){
             res.json({
                 "msg":error.message,
@@ -44,30 +38,23 @@ const historicoLivrariaController = (app,bd) =>{
         }
     })
 
-    app.delete('/historico/id_hist/:id_hist',(req,res)=>{
-        const id_hist = req.params.id_hist
-        historicoDAO.deletaHistorico(id_hist)
-        .then((resposta)=>{
+    app.delete('/historico/id_hist/:id_hist', async (req,res)=>{
+        try{
+            const id_hist = req.params.id_hist
+            const resposta =  await historicoDAO.deletaHistorico(id_hist)
             res.json(resposta)
-        })
-        .catch((erro)=>{
+        }catch(erro){
             res.json(erro)
-        })
+        }
     })
 
-    app.put('/historico/id_hist/:id_hist',(req,res)=>{
+    app.put('/historico/id_hist/:id_hist', async (req,res)=>{
         const id_hist = req.params.id_hist
         const body = req.body
-
         try{
             const histAtualizado = new HistoricoLivraria(body.id_user, body.item, body.preco, body.data_compra)
-            historicoDAO.atualizaHistorico(id_hist,histAtualizado)
-            .then((resposta)=>{
-                res.json(resposta)
-            })
-            .catch((erro)=>{
-                res.json(erro)
-            })
+            const resposta = await historicoDAO.atualizaHistorico(id_hist,histAtualizado)
+            res.json(resposta)
         }catch(erro){
             res.json({
                 "msg":erro.message,
