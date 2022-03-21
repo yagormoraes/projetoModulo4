34c457c161a2f1512935fecb6e14d66d768a0f91
yagormorares <yago.dmoraes@gmail.com>
2022-03-21 13:32:57 -0300

    Adiação do CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors"
 import historicoLivrariaController from "./controller/historicoLivrariaController.js";
 import bd from "./database/sqlite-db.js"; 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 historicoLivrariaController(app,bd)
 
